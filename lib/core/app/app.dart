90f63e348a950accce9118728fceeeda5a888746
Ali <ali.bakhtiyari@fakir-it.de>
2023-02-21 14:14:10 +0330

    feat(app) provide auth service to the widget tree using repo provider from bloc library feat(app) add intl configs and packages

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:makharej_app/features/home/presentation/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:makharej_app/core/theming/theme.dart';
+import 'package:makharej_app/features/authentication/provider/auth_service.dart';
+import 'package:makharej_app/features/splash_screen/ui/splash_screen.dart';
+import 'package:makharej_app/core/navigation/router.dart' as router;
 
 class MakharejApp extends StatelessWidget {
   const MakharejApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Modiriyat E Makharej',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider<AuthService>(
+      create: (_) => AuthService(),
+      child: MaterialApp(
+        title: 'Modiriyat E Makharej',
+        onGenerateRoute: router.Router.generateRoute,
+        theme: themeData,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'),
+        ],
+        home: const SplashScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
