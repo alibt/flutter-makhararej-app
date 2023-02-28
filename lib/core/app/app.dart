5eb663bb885a1d895b7ab8f1058dea23692b0117
Ali <ali.bakhtiyari@fakir-it.de>
2023-02-28 14:09:10 +0330

    feat(app.dart) provide auth bloc and auth provider on root of widget tree

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:makharej_app/core/theming/theme.dart';
-import 'package:makharej_app/features/authentication/provider/auth_service.dart';
+import 'package:makharej_app/features/authentication/provider/auth_provider.dart';
+import 'package:makharej_app/features/authentication/ui/bloc/auth_bloc.dart';
+import 'package:makharej_app/features/categories/provider/categories_provider.dart';
 import 'package:makharej_app/features/splash_screen/ui/splash_screen.dart';
 import 'package:makharej_app/core/navigation/router.dart' as router;
 
@@ -11,21 +13,38 @@ class MakharejApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider<AuthService>(
-      create: (_) => AuthService(),
-      child: MaterialApp(
-        title: 'Modiriyat E Makharej',
-        onGenerateRoute: router.Router.generateRoute,
-        theme: themeData,
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
+    return RepositoryProvider<AuthProvider>(
+      create: (_) => AuthProvider(),
+      child: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<CategoryProvider>(
+            create: (context) => CategoryProvider(
+              context.read<AuthProvider>(),
+            ),
+          ),
         ],
-        supportedLocales: const [
-          Locale('en'),
-        ],
-        home: const SplashScreen(),
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<AuthBloc>(
+                create: (context) => AuthBloc(
+                      RepositoryProvider.of<AuthProvider>(context),
+                    )),
+          ],
+          child: MaterialApp(
+            title: 'Modiriyat E Makharej',
+            onGenerateRoute: router.Router.generateRoute,
+            theme: themeData,
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en'),
+            ],
+            home: const SplashScreen(),
+          ),
+        ),
       ),
     );
   }
